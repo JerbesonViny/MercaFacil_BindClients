e5634489eb89761ac0a543184502ffc5ce6849db
Jerbeson Vinycius <jerbesonvinycius@gmail.com>
2022-06-03 11:43:35 -0300

    Initial commit

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import {Router, Request, Response} from 'express';
+
+
+export const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        "message": "nice"
+    })
+});
